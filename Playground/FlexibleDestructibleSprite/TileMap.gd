ad63e7d9b463fb9e6a55ec519918a625988e57d6
Corentin Le Métayer <corentinlm@corentin-vivobookasuslaptopx570zd>
2021-03-03 21:05:26 +0100

    I lied on the previous commit, now that should be fixed (and i also added some additionnal parameters)

--- Playground/FlexibleDestructibleSprite/TileMap.gd
+++ Playground/FlexibleDestructibleSprite/TileMap.gd
@@ -1,6 +1,9 @@
 extends TileMap
 
 export(String,FILE) var DESTRUCTIBLE_SPRITE_PATH = "res://FlexibleDestructibleSprite/blocks/DestructibleSprite.tscn"
+export(String,FILE) var BLOCK_PATH = "res://FlexibleDestructibleSprite/blocks/BlockArea.tscn" 
+export(int) var TILE_SIZE_POW = 8
+export(int) var MIN_SIZE_POW = 2
 
 var viewport
 
@@ -9,9 +12,10 @@ func _ready():
 	var TileMapRect = computeTileMapBounds()
 	var TileMapPosition = position
 	viewport = createViewport(TileMapPosition,TileMapRect.size)
-	addTilemapToViewport(TileMapRect.size)
+	addTilemapToViewport(TileMapRect)
 	yield(VisualServer, 'frame_post_draw') # waits one frame before getting the texture, otherwise, some unexpected behaviours happens
-	createDestructibleSprite(TileMapPosition)
+	# Note : TileMapRect.position is the offset from the center position to the first pixel on the top left
+	createDestructibleSprite(TileMapRect.position)
 	get_parent().get_parent().get_parent().remove_child(get_parent().get_parent())
 #	get_parent().get_parent().call_deferred("queue_free") # FIXME : The texture of the viewport is linked in memory to the one in the sprite, so freeing the viewport will free the texture
 
@@ -20,19 +24,22 @@ func createDestructibleSprite(pos):
 	var destructible_instance = load(DESTRUCTIBLE_SPRITE_PATH).instance()
 	var img = viewport.get_texture()
 	img.get_data().flip_y()
-#	img.get_data().save_png("<Path to save image>") # debug
+	img.get_data().save_png("res://FlexibleDestructibleSprite/test.png") # debug
 	destructible_instance.position = pos
 	destructible_instance.SPRITE = img
+	destructible_instance.BLOCK_PATH = BLOCK_PATH
+	destructible_instance.TILE_SIZE_POW = TILE_SIZE_POW
+	destructible_instance.MIN_SIZE_POW = MIN_SIZE_POW
 	get_parent().get_parent().get_parent().add_child(destructible_instance)
 	destructible_instance.connect("destructible_sprite_loaded",get_tree().get_current_scene(),"destructibleLoaded")
 	destructible_instance.parseSprite()
 
 # adds tilemap as a child of the viewport
-func addTilemapToViewport(size):
+func addTilemapToViewport(tilemapRect):
 	get_parent().call_deferred("remove_child",self)
 	viewport.call_deferred("add_child", self)
-	position.x = size.x/2
-	position.y = 0
+	# offsets the tilemap position to the first pixel on the top left
+	position = Vector2(0,0) - tilemapRect.position
 
 # function by CKO on godot forum : https://godotengine.org/qa/3276/tilemap-size
 func computeTileMapBounds():
@@ -49,6 +56,7 @@ func createViewport(position,size):
 	viewport = Viewport.new()
 	viewport.transparent_bg = true
 	viewport.size = size
+	viewport.set_usage(0)
 	viewport.render_target_v_flip = true
 	viewportC.add_child(viewport)
 	get_parent().call_deferred("add_child",viewportC)
